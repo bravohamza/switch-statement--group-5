c26c2b322a95e8359e0f5a404c2bb9610333c761
bravohamzajr <bravohamzajr@gmail.com>
2024-05-05 13:53:57 +0300

    complete switch case statement assignment

---
+++ index.js
@@ -0,0 +1,44 @@
+// switch statement
+// Set the student's grade
+
+// const grade = 1;
+
+// switch (true) {
+// 	// If score is 90 or greater
+// 	case grade >= 90:
+// 		console.log("A");
+// 		break;
+// 	// If score is 80 or greater
+// 	case grade >= 80:
+// 		console.log("B");
+// 		break;
+// 	// If score is 70 or greater
+// 	case grade >= 70:
+// 		console.log("C");
+// 		break;
+// 	// If score is 60 or greater
+// 	case grade >= 60:
+// 		console.log("D");
+// 		break;
+// 	// Anything 59 or below is failing
+// 	default:
+// 		console.log("F");
+// }
+
+let day = "Tuesday";
+
+switch (day) {
+  case "Monday":
+    console.log("It's Monday!");
+    break;
+  case "Tuesday":
+    console.log("It's Tuesday!");
+    break;
+  case "Wednesday":
+    console.log("It's Wednesday!");
+    break;
+  default:
+    console.log("It's another day of the week.");
+    break;
+}
+  
